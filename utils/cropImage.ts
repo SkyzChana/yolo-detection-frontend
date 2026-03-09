1f8b803604477aad3a8cc2384f4f3dc3c905ab3c
SkyzChana <kayminerlll@gmail.com>
2026-03-09 16:27:10 +0700

    Test_crop

--- utils/cropImage.ts
+++ utils/cropImage.ts
@@ -46,18 +46,15 @@ export async function getCroppedImg(imageSrc: string, crop: any) {
   const canvas = document.createElement("canvas")
   const ctx = canvas.getContext("2d")
 
-  const scaleX = image.naturalWidth / image.width
-  const scaleY = image.naturalHeight / image.height
-
   canvas.width = crop.width
   canvas.height = crop.height
 
   ctx?.drawImage(
     image,
-    crop.x * scaleX,
-    crop.y * scaleY,
-    crop.width * scaleX,
-    crop.height * scaleY,
+    crop.x,
+    crop.y,
+    crop.width,
+    crop.height,
     0,
     0,
     crop.width,
